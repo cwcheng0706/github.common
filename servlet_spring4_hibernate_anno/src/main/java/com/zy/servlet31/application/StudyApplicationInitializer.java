31c4e80df845a5bcf6dda46191902086dab42eaa
zy-pc <654821567@qq.com>
2014-09-19 22:51:00 +0800

    首次提交

---
+++ servlet_spring4_hibernate_anno/src/main/java/com/zy/servlet31/application/StudyApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.zy.servlet31.application;
+
+
+import javax.servlet.ServletContext;
+
+
+public interface StudyApplicationInitializer {
+
+	public void onStartup(ServletContext ctx);
+}
