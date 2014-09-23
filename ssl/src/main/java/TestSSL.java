df1aab59330ce6d07b112106d93f9ede1fd37355
zy-JL <654821567@qq.com>
2014-09-23 18:46:59 +0800

    修改bug

--- ssl/src/main/java/TestSSL.java
+++ ssl/src/main/java/TestSSL.java
@@ -4,16 +4,21 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class TestSSL {
@@ -25,8 +30,11 @@ public class TestSSL {
 	// https端口
 	private static final int HTTPS_PORT = 443;
 
-	// 请求的接口地址
-	private static final String HTTPS_URL = "https://open.jlfex.com/test/rest?method=jl.order.do&timestamp=2014-09-23%2017:30:03&v=1.0&serial_no=99100030006200000461&name=test&certiNum=123456&financeProductId=10001&orderAmt=25000";
+	// 请求的接口地址post方式url
+	private static final String HTTPS_URL = "https://open.jlfex.com/test/rest?method=jl.order.do" ;
+	
+	// 请求的接口地址get方式url
+//	private static final String HTTPS_URL = "https://open.jlfex.com/test/rest?method=jl.financefroduct.get&pageSize=10&pageNum=1&timestamp=2014-09-23%2017:30:03&serial_no=9910013000620000046&v=1.0";
 
 	// 用户申请的证书
 	private static final String KEY_STORE_CLIENT_PATH = "C:\\testssl\\006.p12";
@@ -52,6 +60,10 @@ public class TestSSL {
 	public static String ssl() throws Exception {
 		String ret = "";
 		HttpClient httpClient = new DefaultHttpClient();
+		
+		InputStreamReader isr = null;
+		BufferedReader bufferedReader = null;
+		
 		try {
 			KeyStore keyStore = KeyStore.getInstance(KEY_STORE_TYPE_P12);
 			KeyStore trustStore = KeyStore.getInstance(KEY_STORE_TYPE_JKS);
@@ -77,34 +89,51 @@ public class TestSSL {
 			Scheme sch = new Scheme(SCHEME_HTTPS, HTTPS_PORT, socketFactory);
 			httpClient.getConnectionManager().getSchemeRegistry().register(sch);
 
-			// HttpGet get = new HttpGet(HTTPS_URL);
+			
 			HttpPost post = new HttpPost(HTTPS_URL);
-			StringEntity reqEntity = new StringEntity("<html>你好啊啊</html>", "UTF-8");
-			post.setEntity(reqEntity);
-
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();  
+	        nvps.add(new BasicNameValuePair("timestamp", "2014-09-23 18:30:03"));  
+	        nvps.add(new BasicNameValuePair("v", "1.0"));  
+	        nvps.add(new BasicNameValuePair("serial_no", "9910013000620000046"));
+	        nvps.add(new BasicNameValuePair("name", "test"));
+	        nvps.add(new BasicNameValuePair("certiNum", "123456"));
+	        nvps.add(new BasicNameValuePair("financeProductId", "10001"));
+	        nvps.add(new BasicNameValuePair("orderAmt", "25000"));
+	        post.setEntity(new UrlEncodedFormEntity(nvps)); 
+	        
 			HttpResponse response = httpClient.execute(post);
+			
+			//get方式
+//			HttpGet get = new HttpGet(HTTPS_URL);
+//			HttpResponse response = httpClient.execute(get);
+			
 			HttpEntity respEntity = response.getEntity();
 
-			System.out.println(response.getStatusLine());
 
-			StringBuilder sb = new StringBuilder();
+			StringBuffer sb = new StringBuffer();
+			String text = "";
 			if (respEntity != null) {
 				System.out.println("Response content length: " + respEntity.getContentLength());
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(respEntity.getContent(), "UTF-8"));
-				String text;
-
+				isr = new InputStreamReader(respEntity.getContent(), "UTF-8");
+				bufferedReader = new BufferedReader(isr);
 				while ((text = bufferedReader.readLine()) != null) {
 					sb.append(text);
 				}
-				bufferedReader.close();
+				
+				ret = sb.toString();
 
 			}
-			System.out.println("----------------------------------------");
-			ret = sb.toString();
-			System.out.println("----------------------------------------");
 			EntityUtils.consume(respEntity);
+			
+			System.out.println("-------------------响应start---------------------");
+			System.out.println(ret);
+			System.out.println("-------------------响应end---------------------");
+		}catch(Exception e){
+			e.printStackTrace();
 		} finally {
+			bufferedReader.close();
 			httpClient.getConnectionManager().shutdown();
+			
 		}
 
 		return ret;
