8b8b4521fcef83d53697c76fbcc02d946c7e3e20
zy-JL <654821567@qq.com>
2014-09-28 16:45:30 +0800

    更新

--- study/src/main/java/com/zy/zookeeper/government/Test.java
+++ study/src/main/java/com/zy/zookeeper/government/Test.java
@@ -2,27 +2,36 @@ package com.zy.zookeeper.government;
 
 import java.util.Random;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.zookeeper.KeeperException;
 
 public class Test {
 
+	private static Log logger = LogFactory.getLog(Test.class);
+
+	public static String root = "/app1/barrier";
+	public static String address = "172.16.2.199:2181,172.16.2.199:2182,172.16.2.199:2183";
+	public static int barrierSize = 3;
+
 	public static void main(String args[]) {
-		if (args[0].equals("qTest"))
-			queueTest(args);
-		else
-			barrierTest(args);
+		// queueTest(args);
+
+		for (int i = 0; i < barrierSize; i++) {
+			barrierTest(i + "");
+		}
 
 	}
 
 	public static void queueTest(String args[]) {
 		Queue q = new Queue(args[1], "/app1");
 
-		System.out.println("Input: " + args[1]);
+		logger.debug("Input: " + args[1]);
 		int i;
 		Integer max = new Integer(args[2]);
 
 		if (args[3].equals("p")) {
-			System.out.println("Producer");
+			logger.debug("Producer");
 			for (i = 0; i < max; i++)
 				try {
 					q.produce(10 + i);
@@ -32,12 +41,12 @@ public class Test {
 
 				}
 		} else {
-			System.out.println("Consumer");
+			logger.debug("Consumer");
 
 			for (i = 0; i < max; i++) {
 				try {
 					int r = q.consume();
-					System.out.println("Item: " + r);
+					logger.debug("Item: " + r);
 				} catch (KeeperException e) {
 					i--;
 				} catch (InterruptedException e) {
@@ -47,37 +56,45 @@ public class Test {
 		}
 	}
 
-	public static void barrierTest(String args[]) {
-		Barrier b = new Barrier(args[1], "/b1", new Integer(args[2]));
-		try {
-			boolean flag = b.enter();
-			System.out.println("Entered barrier: " + args[2]);
-			if (!flag)
-				System.out.println("Error when entering the barrier");
-		} catch (KeeperException e) {
+	public static void barrierTest(final String nodeName) {
+		new Thread(new Runnable() {
 
-		} catch (InterruptedException e) {
+			@Override
+			public void run() {
 
-		}
+				Barrier b = new Barrier(address, root, new Integer(barrierSize));
+				try {
+					boolean flag = b.enter(nodeName);
+					logger.debug("Entered barrier: " + barrierSize);
+					if (!flag) {
+						logger.debug("Error when entering the barrier");
+					}
+				} catch (Exception e) {
+					logger.error(e);
+				}
 
-		// Generate random integer
-		Random rand = new Random();
-		int r = rand.nextInt(100);
-		// Loop for rand iterations
-		for (int i = 0; i < r; i++) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
+				// Generate random integer
+				Random rand = new Random();
+				int r = rand.nextInt(100);
+				// Loop for rand iterations
+				for (int i = 0; i < r; i++) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						logger.error(e);
+					}
+				}
+				try {
+					b.leave(nodeName);
+				} catch (KeeperException e) {
+
+				} catch (InterruptedException e) {
+
+				}
+				logger.debug("Left barrier");
 
 			}
-		}
-		try {
-			b.leave();
-		} catch (KeeperException e) {
+		},nodeName).start();
 
-		} catch (InterruptedException e) {
-
-		}
-		System.out.println("Left barrier");
 	}
 }
