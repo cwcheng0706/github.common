b6cf9754418e671925a7650a526b95c2db444c99
zy-JL <654821567@qq.com>
2014-10-22 16:55:17 +0800

    d

---
+++ study/src/main/java/com/zy/security/ValidataKeyLength.java
@@ -0,0 +1,33 @@
+package com.zy.security;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+/**
+ * 验证生成密钥的长度
+ * @Project: study
+ * @Author zy
+ * @Company: JL
+ * @Create Time: 2014年10月21日 下午4:44:31
+ */
+public class ValidataKeyLength {
+
+	public static void main(String[] args) throws Exception {
+		
+		KeyGenerator generator = KeyGenerator.getInstance("DES");
+		generator.init(256);
+		SecretKey secretKey = generator.generateKey();
+		
+		byte[] key = secretKey.getEncoded();
+		
+		/**
+		 * 由于美国出口的限制，只支持56位长度的密钥，这样安全性就比较低。如果你所在的国家和地区允许的话，可以获得相应的权限，就得去官方去下载JCE包
+		 * Exception in thread "main" java.security.InvalidParameterException: Wrong keysize: must be equal to 56
+				at com.sun.crypto.provider.DESKeyGenerator.engineInit(DESKeyGenerator.java:90)
+				at javax.crypto.KeyGenerator.init(KeyGenerator.java:501)
+				at javax.crypto.KeyGenerator.init(KeyGenerator.java:478)
+				at com.zy.cert.ValidataKeyLength.main(ValidataKeyLength.java:18)
+
+		 */
+	}
+}
