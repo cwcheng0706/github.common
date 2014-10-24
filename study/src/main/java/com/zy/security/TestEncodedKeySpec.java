68b3f3091fa8b3712ec990291eefccc180cba672
zy-JL <654821567@qq.com>
2014-10-24 09:09:01 +0800

    。。

---
+++ study/src/main/java/com/zy/security/TestEncodedKeySpec.java
@@ -0,0 +1,38 @@
+package com.zy.security;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+public class TestEncodedKeySpec {
+	
+	public static void main(String[] args) throws Exception{
+		
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+
+		generator.initialize(2048);
+		KeyPair keyPair = generator.generateKeyPair();
+		PrivateKey privateKeyPair = keyPair.getPrivate();
+		byte[] privateBytes = privateKeyPair.getEncoded();
+		
+		
+		
+		
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		KeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateBytes);
+		Key privateKey = keyFactory.generatePrivate(privateKeySpec);
+		
+		
+		//目前没用
+		FileOutputStream fos = new FileOutputStream(new File("c:\\test.p12"));
+		fos.write(privateBytes);
+		fos.flush();
+	}
+
+}
