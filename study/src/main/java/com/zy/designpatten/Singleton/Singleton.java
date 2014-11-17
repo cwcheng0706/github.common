44498c2b9eac9ad0c8d8a5d3711c9abfa0205eae
zy-pc <654821567@qq.com>
2014-11-17 09:32:14 +0800

    修改包路径

---
+++ study/src/main/java/com/zy/designpatten/Singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.zy.designpatten.Singleton;
+
+/**
+ * 采用静态内部类 实现单例模式
+ * @Project: study
+ * @Author zy
+ * @Company: JL
+ * @Create Time: 2014年11月17日 上午9:29:53
+ */
+public class Singleton {
+
+	private Singleton() {
+	}
+
+	public Singleton getInstance() {
+		return SingletonHolder.instance;
+	}
+
+	private static class SingletonHolder {
+		private static Singleton instance = new Singleton();
+	}
+}
