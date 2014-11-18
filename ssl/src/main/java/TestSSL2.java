97843abaaebe789bafd9f3fad01469ed0f3588c2
zy-pc <654821567@qq.com>
2014-11-18 13:01:10 +0800

    .

--- ssl/src/main/java/TestSSL2.java
+++ ssl/src/main/java/TestSSL2.java
@@ -30,21 +30,21 @@ public class TestSSL2 {
 	private static final String SCHEME_HTTPS = "https";
 
 	// https端口
-	private static final int HTTPS_PORT = 8443;
+	private static final int HTTPS_PORT = 443;
 
 	// 请求的接口地址post方式url
-	private static final String HTTPS_URL = "https://172.16.2.2/study/dynamicServlet" ;
+	private static final String HTTPS_URL = "https://ssl.jl.com/study/dynamicServlet" ;
 	
 	// 请求的接口地址get方式url
 //	private static final String HTTPS_URL = "https://open.jlfex.com/test/rest?method=jl.financefroduct.get&pageSize=10&pageNum=1&timestamp=2014-09-23%2017:30:03&serial_no=9910013000620000046&v=1.0";
 
 	// 用户申请的证书
-	private static final String KEY_STORE_CLIENT_PATH = "D:\\ssl\\2.199\\openssl\\twoCA\\user.p12";
+	private static final String KEY_STORE_CLIENT_PATH = "D:\\ssl\\79.110\\zhuyong001.p12";
 	// p12文件的密码在用户下载包里面的txt文件中
 	private static final String KEY_STORE_PASSWORD = "user123456";
 
 	// 用户下载包里面服务端证书文件（server.cer） 需要按文档步骤生成服务端的信任证书库文件
-	private static final String KEY_STORE_TRUST_PATH = "D:\\ssl\\2.199\\openssl\\twoCA\\server.truststore";
+	private static final String KEY_STORE_TRUST_PATH = "D:\\ssl\\79.110\\server.truststore";
 	// 在作生成server.truststore操作时自己设置的密码
 	private static final String KEY_STORE_TRUST_PASSWORD = "server123456";
 
@@ -87,7 +87,7 @@ public class TestSSL2 {
 				}
 			}
 			SSLSocketFactory socketFactory = new SSLSocketFactory(keyStore, KEY_STORE_PASSWORD, trustStore);
-			socketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+//			socketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 
 			Scheme sch = new Scheme(SCHEME_HTTPS, HTTPS_PORT, socketFactory);
 			httpClient.getConnectionManager().getSchemeRegistry().register(sch);
@@ -129,6 +129,7 @@ public class TestSSL2 {
 			EntityUtils.consume(respEntity);
 			
 			System.out.println("-------------------响应start---------------------");
+			System.out.println(response.getStatusLine().getStatusCode());
 			System.out.println(ret);
 			System.out.println("-------------------响应end---------------------");
 		}catch(Exception e){
