ffe77d57bc257e0d655b685c07b909f73be8860f
zhuyong <zhuyong@jinlufund.com>
2015-03-20 15:58:32 +0800

    新增16转2进制

--- study/src/main/java/com/zy/security/Coder.java
+++ study/src/main/java/com/zy/security/Coder.java
@@ -9,21 +9,21 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 public abstract class Coder {
-	
+
 	public static final String ALGORITHM_DES = "DES";
 	public static final String ALGORITHM_3DES = "DESSede";
 
 	static {
 		Security.addProvider(new BouncyCastleProvider());
-		
+
 		System.out.println("============静态加载BouncyCastleProvider start================");
 		Provider[] ps = Security.getProviders();
-		for(int i = 0 ; i < ps.length ; i++ ) {
+		for (int i = 0; i < ps.length; i++) {
 			System.out.println(ps[i].getName() + "  === " + ps[i].getInfo());
 		}
 		System.out.println("============静态加载BouncyCastleProvider end ================\n");
 	}
-	
+
 	/**
 	 * BASE64解密
 	 * 
@@ -44,5 +44,25 @@ public abstract class Coder {
 	 */
 	public static String encryptBASE64(byte[] key) throws Exception {
 		return (new BASE64Encoder()).encodeBuffer(key);
+	}
+
+	/**
+	 * 十六进制（去掉0x开头）转二进制
+	 * @Author zy
+	 * @Company: 
+	 * @Create Time: 2015年3月20日 下午3:57:43
+	 * @param hexStr
+	 * @return
+	 */
+	public static byte[] parseHexStr2Byte(String hexStr) {
+		if (hexStr.length() < 1)
+			return null;
+		byte[] result = new byte[hexStr.length() / 2];
+		for (int i = 0; i < hexStr.length() / 2; i++) {
+			int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+			int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+			result[i] = (byte) (high * 16 + low);
+		}
+		return result;
 	}
 }
