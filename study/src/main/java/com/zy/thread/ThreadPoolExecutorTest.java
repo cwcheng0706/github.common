7240762ba19ccd453324a71d84db555ffe5675b2
zy <zy@DESKTOP-K4AVVFL>
2016-07-21 18:25:49 +0800

    修一把

--- study/src/main/java/com/zy/thread/ThreadPoolExecutorTest.java
+++ study/src/main/java/com/zy/thread/ThreadPoolExecutorTest.java
@@ -12,29 +12,43 @@ public class ThreadPoolExecutorTest {
 	static BlockingQueue<Runnable> workQueue = null;
 
 	public static void main(String[] args) throws Exception {
-		test1();
+		
+		int corePoolSize = 2;
+		int maximumPoolSize = 4;
+		long keepAliveTime = 60;
+		TimeUnit unit = TimeUnit.SECONDS;
+		workQueue = new ArrayBlockingQueue<Runnable>(4);
+//		workQueue = new LinkedBlockingQueue<Runnable>();
+		executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
 
+		
+		test1();
 		moitor();
+
 
 	}
 
-	public static void test1() {
-		int corePoolSize = 4;
-		int maximumPoolSize = 100;
-		long keepAliveTime = 60;
-		TimeUnit unit = TimeUnit.SECONDS;
-//		workQueue = new ArrayBlockingQueue<Runnable>(40);
-		workQueue = new LinkedBlockingQueue<Runnable>();
-		
+	public static void test1() throws Exception {
 
-		executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+		Thread t = new Thread(new Runnable() {
+			public void run() {
 
-		for (int i = 0; i < 200; i++) {
-			System.out.println("execute " + "Thread_" + i);
-			executor.submit(new RunnableWorker<Object>("Thread_" + i));
-		}
-		
-		System.out.println("");
+				for (int i = 0; i < 8; i++) {
+					System.out.println("execute " + "Thread_" + i);
+					executor.submit(new RunnableWorker<Object>("Thread_" + i));
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				System.out.println("");
+
+			}
+		});
+	
+		t.start();
 	}
 
 	public static void moitor() {
@@ -49,7 +63,7 @@ public class ThreadPoolExecutorTest {
 					long taskCount = executor.getTaskCount();
 					long comCount = executor.getCompletedTaskCount();
 
-					System.out.println("activeCount【" + activeCount + "】" + " poolSize【" + poolSize + "】" + "  comCount【" + comCount + "】" + "  taskCount【" + taskCount + "】");
+					System.out.println("activeCount【" + activeCount + "】" + " poolSize【" + poolSize + "】" + "  completeCount【" + comCount + "】" + "  taskCount【" + taskCount + "】  queue:【" + workQueue.size() + "】");
 					try {
 						Thread.sleep(1500);
 					} catch (InterruptedException e) {
@@ -75,14 +89,14 @@ class RunnableWorker<T> implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println(name + "【" + Thread.currentThread().getName() + "】 work start..");
+//		System.out.println(name + "【" + Thread.currentThread().getName() + "】 work start..");
 
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(100000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println(name + "【" + Thread.currentThread().getName() + "】 work end..");
+//		System.out.println(name + "【" + Thread.currentThread().getName() + "】 work end..");
 	}
 
 }
